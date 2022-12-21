83c961eda330fdd3343ec86565f00c26f25118e5
Julien Capellari <julien.capellari@gmail.com>
2022-12-21 01:24:57 +0100

    feat: rework cellular step

--- src/generators/steps/cellular.step.ts
+++ src/generators/steps/cellular.step.ts
@@ -7,72 +7,48 @@ import { CachedStep } from './cached-step';
 
 // Constants
 const DIRECTIONS = [
+  vector(-1, -1),
+  vector(0, -1),
+  vector(1, -1),
+  vector(-1, 0),
+  vector(1, 0),
+  vector(-1, 1),
   vector(0, 1),
   vector(1, 1),
-  vector(1, 0),
-  vector(1, -1),
-  vector(0, -1),
-  vector(-1, -1),
-  vector(-1, 0),
-  vector(-1, 1),
 ];
 
 // Class
 @injectable()
 export class CellularStep extends CachedStep<undefined> {
   // Methods
-  private async _getNeighbors(pos: Point): Promise<ITile[]> {
-    const toRequest: Point[] = [];
-    const result: ITile[] = [];
-
-    // Read cache
-    for (const dir of DIRECTIONS) {
-      const n = pos.add(dir);
-      const cached = this.tileCache.search(n);
-
-      if (cached.length > 0) {
-        result.push(cached[0]);
-      } else {
-        toRequest.push(n);
-      }
-    }
-
-    // Request missing
-    for (const req of toRequest) {
-      const tile = await this.getPreviousTile(req);
-
-      if (tile) {
-        result.push(tile);
-      }
-    }
-
-    // Clean cache
-    this.tileCache.removeUntil(pos.add({ dx: -1, dy: -1 })); // <= will not be used later
-
-    return result;
-  }
-
   protected async applyOn(pos: Point, base?: ITile): Promise<string> {
-    if (!base) {
-      throw new Error('Cellular generator cannot be the first generator');
-    }
-
-    const neighbors = await this._getNeighbors(pos);
-    const biomes: Record<string, number> = {};
-
-    for (const n of neighbors) {
-      if (!pos.equals(n.pos)) {
-        biomes[n.biome] = (biomes[n.biome] ?? 0) + 1;
+    try {
+      if (!base) {
+        throw new Error('Cellular must not be the first step');
       }
-    }
 
-    // Update tile
-    for (const [biome, cnt] of Object.entries(biomes)) {
-      if (cnt > 4) {
-        return biome;
+      const biomes: Record<string, number> = {};
+
+      for (const dir of DIRECTIONS) {
+        // Get neighbor
+        const tile = await this.getPreviousTile(pos.add(dir));
+
+        if (!tile) {
+          continue;
+        }
+
+        // Compute
+        biomes[tile.biome] = (biomes[tile.biome] ?? 0) + 1;
+
+        if (biomes[tile.biome] > 4) {
+          return tile.biome;
+        }
       }
-    }
 
-    return base.biome;
+      return base.biome;
+    } finally {
+      // Clear cache
+      this.tileCache.removeUntil(pos.add({ dx: -1, dy: -1 })); // <= will not be used later
+    }
   }
 }
